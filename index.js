f85d400b564bc08c93a138645cba6e2d405f2ad4
sanjay E <sanjaye0408@gmail.com>
2025-07-12 13:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ function celsius() {
     document.getElementById("celsius-value").textContent = "In  valid input";
     return;
   }
-
-  document.getElementById("celsius-value").textContent =
-    input.toFixed(2) + "°C";
   const celsius = ((input - 32) * 5) / 9;
   document.getElementById("celsius-value").textContent =
     celsius.toFixed(2) + "°C";
@@ -18,9 +15,6 @@ function fahrenheit() {
     document.getElementById("fahrenheit-value").textContent = "Invalid input";
     return;
   }
-
-  document.getElementById("fahrenheit-value").textContent =
-    input.toFixed(2) + "°F";
   const fahrenheit = (input * 9) / 5 + 32;
   document.getElementById("fahrenheit-value").textContent =
     fahrenheit.toFixed(2) + "°F";
@@ -32,8 +26,6 @@ function kelvin() {
     document.getElementById("kelvin-value").textContent = "Invalid input";
     return;
   }
-
-  document.getElementById("kelvin-value").textContent = input.toFixed(2) + "K";
   const kelvin = input + 273.15;
   document.getElementById("kelvin-value").textContent = kelvin.toFixed(2) + "K";
 }
